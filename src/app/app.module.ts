64dce32cca5e4c82388d4202bb2f800666281c76
Nehe <nehe@Nehes-MacBook-Pro.local>
2017-10-24 15:15:43 +0530

    response in view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdToolbarModule, MdTableModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatTableModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MdToolbarModule,
-    MdTableModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatTableModule,
     HttpModule,
   ],
   providers: [],
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,55 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import { RepoServiceService } from '../repo-service/repo-service.service';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  providers: [RepoServiceService]
 })
 export class ContentComponent implements OnInit {
 
   private url = 'https://api.github.com/repositories?since=862';
-  results: any;
-  private isDataAvailable: boolean;
+  data: any = {};
 
-  constructor(private myHttp: RepoServiceService) { 
+  constructor(private http: Http) { 
+    this.getData();
+    this.getContacts();
   }
 
-  ngOnInit() {
-    this.myHttp.getDataObservable(this.url).subscribe(
-      data => {
-        this.results = JSON.stringify(data);
-        //console.log("I CANT SEE DATA HERE: ", this.results);
-      }
-    );
+  ngOnInit() {}
+
+  getData() {
+    return this.http.get(this.url)
+    .map((res: Response) => res.json())
   }
 
-  //displayedColumns = ['id', 'repoName', 'repoDesc', 'ownerName', 'type'];
-  //dataSource = new ExampleDataSource();
+  getContacts() {
+    this.getData().subscribe(data => {
+      this.data = data;
+      console.log(data);
+    })
+  }
 }
 
-// export interface Element  {
-//   repoName: string;
-//   id: number;
-//   ownerName: string;
-//   repoDesc: string;
-//   type: string; 
-// }
-
-// const data: Element[] = [
-//   {id: 1, repoName: 'Hydrogen', repoDesc: 'xcv', ownerName:'vishakha', type: 'admin'},
-//   {id: 2, repoName: 'Helium', repoDesc: 'xcv', ownerName: 'ram', type: 'He'},
-//   {id: 3, repoName: 'Lithium', repoDesc: 'xcv', ownerName: 'sita', type: 'Li'},
-//   {id: 4, repoName: 'Beryllium', repoDesc: 'xcv', ownerName: 'kishore', type: 'Be'},
-//  ];
-
-// export class ExampleDataSource extends DataSource<any> {
-//   connect(): Observable<Element[]> {
-//     return Observable.of(data);
-//   }
-//   disconnect() {}
-// }
